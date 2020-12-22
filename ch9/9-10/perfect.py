20dec82446ea072a5720ade982b64a53d4d8cdfd
tzyLee <b06901145@ntu.edu.tw>
2020-12-23 01:22:13 +0800

    Remove extra lines in perfect.py

--- ch9/9-10/perfect.py
+++ ch9/9-10/perfect.py
@@ -3,9 +3,7 @@ def isPrime(num):
         return False
     if num % 2 == 0:
         return num == 2
-    check = 0
     for factor in range(3, int(num**0.5 + 1), 2):
-        check += 1
         if num % factor == 0:
             return False
     return True
@@ -14,7 +12,7 @@ if __name__ == "__main__":
     found = 0
     perfects = []
     twoPowerP = 2
-    while found < 9:
+    while found < 8:
         if isPrime(twoPowerP -1):
             found += 1
             perfects.append(twoPowerP//2 * (twoPowerP - 1))
